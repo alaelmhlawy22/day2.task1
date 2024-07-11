c5b144c92facfa396be8d8dd3a5426edeede5dd9
Active Group <Active Group@DESKTOP-46BDU29>
2024-07-11 14:04:35 +0300

    Add project files.

---
+++ first_project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($" {num} * {i} = {num * i}");
+            }
+        }
+    }
+}
